ca83f7efb48ab964871d09e528c8a4e0ec4cd0e0
陈庆 <chenqing873@1caifu.com>
2019-10-23 22:31:04 +0800

    first

---
+++ testLib/testLib/testLib.h
@@ -0,0 +1,19 @@
+//
+//  testLib.h
+//  testLib
+//
+//  Created by 陈庆 on 2019/10/23.
+//  Copyright © 2019 陈庆. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testLib.
+FOUNDATION_EXPORT double testLibVersionNumber;
+
+//! Project version string for testLib.
+FOUNDATION_EXPORT const unsigned char testLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testLib/PublicHeader.h>
+
+
